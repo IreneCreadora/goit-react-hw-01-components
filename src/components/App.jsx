247e02b29ed76f0a1e8f88a464f07187936a97a4
IreneCreadora <nargiza.dancer@gmail.com>
2022-11-09 20:45:43 +0200

    add profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { Profile } from 'components/Profile/Profile';
+// import { Statistics } from 'components/Statistics/Statistics';
+// import { FriendList } from 'components/FriendList';
+// import { TransactionHistory } from 'components/TransactionHistory';
+
+import user from '../received/user';
+// import data from '../received/data';
+// import friends from '../received/friends';
+// import transactions from '../received/transactions';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} /> */}
+      {/* <FriendList></FriendList>
+      <TransactionHistory></TransactionHistory> */}
+    </>
   );
 };
